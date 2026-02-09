e2ed3b1c3bccb98b0de7828f4c322bb199a4dae0
AndriySymon <ipz235_sag@student.ztu.edu.ua>
2026-02-09 21:31:00 +0200

    Додано ReadMe файл, License та репорт до неї та Cookie popup

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,26 @@ import SettingsPage from "./pages/SettingsPage";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { theme } from "./styles/theme";
+import CookieConsent from "react-cookie-consent";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <CookieConsent
+        location="bottom"
+        enableDeclineButton
+        buttonText="Accept"
+        declineButtonText="Decline"
+        cookieName="fifteenPuzzleCookieConsent"
+        style={{ background: "#1f1f1f" }}
+        buttonStyle={{ color: "#000", fontSize: "14px" }}
+        declineButtonStyle={{ fontSize: "14px" }}
+        expires={150}
+      >
+        This website uses cookies to improve user experience and complies with
+        GDPR regulations.
+      </CookieConsent>
         <BrowserRouter>
           <div className="app">
             <Routes>
